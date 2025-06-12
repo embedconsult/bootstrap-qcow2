4e0d6a12763c8043af29e526a83b9f329cd66900
jkridner <>
2025-06-12 16:48:40 +0000

    Add idea of checking dependencies
    FossilOrigin-Name: dab86f98b247f0eeef5c4811d7728c2f58b172274165394c9ea01347cff7232a

--- spec/bootstrap-qcow2_spec.cr
+++ spec/bootstrap-qcow2_spec.cr
@@ -3,5 +3,11 @@ require "./spec_helper"
 describe Bootstrap::Qcow2 do
   it "can be instantiated" do
     qcow2 = Bootstrap::Qcow2.new("blabl-space-20250612.qcow2")
+    qcow2.should be_a(Bootstrap::Qcow2)
+  end
+
+  it "can check if it has dependencies" do
+    qcow2 = Bootstrap::Qcow2.new("blabl-space-20250612.qcow2")
+    qcow2.checkDeps.should be_true
   end
 end
--- src/bootstrap-qcow2.cr
+++ src/bootstrap-qcow2.cr
@@ -6,5 +6,11 @@ module Bootstrap
     def initialize(@filename : String)
       puts "Working with qcow2 file: #{@filename}"
     end
+
+    def checkDeps()
+      File.exists?("../fossil-scm.fossil") &&
+      File.exists?("../bootstrap-qcow2.fossil") &&
+      true
+    end
   end
 end
