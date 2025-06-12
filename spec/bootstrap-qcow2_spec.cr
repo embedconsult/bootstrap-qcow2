5b83fa8a91ed21a3c7e4e580913319e92ca719ee
jkridner <>
2025-06-12 17:09:32 +0000

    Start using logging and add search for executables
    FossilOrigin-Name: 46bccfd8fadfe495ad28a3ec15de2657711eafede372fbc4e50f939d797327c8

--- spec/bootstrap-qcow2_spec.cr
+++ spec/bootstrap-qcow2_spec.cr
@@ -1,4 +1,7 @@
 require "./spec_helper"
+require "log"
+
+Log.setup_from_env
 
 describe Bootstrap::Qcow2 do
   it "can be instantiated" do
@@ -9,5 +12,11 @@ describe Bootstrap::Qcow2 do
   it "can check if it has dependencies" do
     qcow2 = Bootstrap::Qcow2.new("blabl-space-20250612.qcow2")
     qcow2.checkDeps.should be_true
+  end
+
+  it "has a helper to find executalbes" do
+    qimg = Bootstrap::Qcow2.findExe("qemu-img")
+    qimg.should be_a(String)
+    Log.info { "Found qemu-image at #{qimg}" }
   end
 end
--- src/bootstrap-qcow2.cr
+++ src/bootstrap-qcow2.cr
@@ -1,16 +1,22 @@
 # TODO: Write documentation for `Bootstrap::Qcow2`
+require "log"
+
 module Bootstrap
   VERSION = "0.1.0"
 
   class Qcow2
     def initialize(@filename : String)
-      puts "Working with qcow2 file: #{@filename}"
+      Log.info { "Working with qcow2 file: #{@filename}" }
     end
 
     def checkDeps()
       File.exists?("../fossil-scm.fossil") &&
       File.exists?("../bootstrap-qcow2.fossil") &&
       true
+    end
+
+    def self.findExe(exeName : String)
+      Process.find_executable(exeName)
     end
   end
 end
