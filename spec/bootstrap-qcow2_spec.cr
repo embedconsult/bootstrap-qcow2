d96a5223c24f76e55579cc151a25e5662fb32923
jkridner <>
2025-06-12 16:36:14 +0000

    Write initialization spec
    FossilOrigin-Name: e21aa1d3f74a7291626bd65b7b57a9b8b65c7174faad516e18c702becba7f316

--- spec/bootstrap-qcow2_spec.cr
+++ spec/bootstrap-qcow2_spec.cr
@@ -1,9 +1,7 @@
 require "./spec_helper"
 
 describe Bootstrap::Qcow2 do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
+  it "can be instantiated" do
+    qcow2 = Bootstrap::Qcow2.new("blabl-space-20250612.qcow2")
   end
 end
--- src/bootstrap-qcow2.cr
+++ src/bootstrap-qcow2.cr
@@ -1,6 +1,10 @@
 # TODO: Write documentation for `Bootstrap::Qcow2`
-module Bootstrap::Qcow2
+module Bootstrap
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  class Qcow2
+    def initialize(@filename : String)
+      puts "Working with qcow2 file: #{@filename}"
+    end
+  end
 end
