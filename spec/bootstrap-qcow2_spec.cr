e6423b093826607195624948f9a3e3b45c02872f
jkridner <>
2025-06-12 16:23:04 +0000

    Create using crystal init app
    FossilOrigin-Name: d51d3eb37213f35fb1d04b0f88e0b7c601c5e4f3398c6c9d146a6a8c2ce8d845

---
+++ spec/bootstrap-qcow2_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe Bootstrap::Qcow2 do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ spec/spec_helper.cr
@@ -0,0 +1,2 @@
+require "spec"
+require "../src/bootstrap-qcow2"
