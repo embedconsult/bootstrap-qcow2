4aaae1757fab72a7a44fd283314ad24a0ba1aecd
jkridner <>
2025-07-16 06:58:48 +0000

    Try KVM and bootx86.efi as lower-case
    FossilOrigin-Name: 7d3d5066c30c2f8528d121b6f84e6eaa2a1fe6ce795e27d10e96dc91db716b6d

--- data/run_qemu.sh
+++ data/run_qemu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $(dirname $0)
-qemu-system-x86_64 -m 1G -bios ./bios.bin -nographic -drive file=blabl-space-20250612.qcow2
+qemu-system-x86_64 -cpu host -enable-kvm -m 1G -bios ./bios.bin -nographic -drive file=blabl-space-20250612.qcow2
