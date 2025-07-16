f9c85b7a59bf6688314bf14fd399159696282d32
jkridner <>
2025-07-16 17:59:11 +0000

    Still trying
    FossilOrigin-Name: b4d7c92bbd0aba42b5e2d3a48250acf7693fc003d9bdf95d7d2c5a34551917ac

--- data/run_qemu.sh
+++ data/run_qemu.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 cd $(dirname $0)
-qemu-system-x86_64 -cpu host -enable-kvm -m 1G -bios ./bios.bin -nographic -drive file=blabl-space-20250612.qcow2
+qemu-system-x86_64 \
+	-cpu host -enable-kvm -smp 2 -m 1G \
+	-nographic \
+	-bios ./bios.bin \
+	-drive file=blabl-space-20250612.qcow2 \
+	-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+	-chardev pty,id=char0 -serial chardev:char0
+
