9371dae6a9b86768f542161297762ae507fe63b6
jkridner <>
2025-07-17 00:52:15 +0000

    No use of KVM/CPU on non x86-64 host for now
    FossilOrigin-Name: bebed5f9b4811b5505620fabb19191a7478c5c5f1ce42b5d0a742e5e183c5e71

--- data/run_qemu.sh
+++ data/run_qemu.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 cd $(dirname $0)
-qemu-system-x86_64 \
-	-cpu host -enable-kvm -smp 2 -m 1G \
-	-nographic \
-	-bios ./bios.bin \
-	-drive file=blabl-space-20250612.qcow2 \
-	-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
-	-chardev pty,id=char0 -serial chardev:char0
-
+if "$(uname -m)" == "x86_64"; then
+	qemu-system-x86_64 \
+		-cpu host -enable-kvm -smp 2 -m 1G \
+		-nographic \
+		-bios ./bios.bin \
+		-drive file=blabl-space-20250612.qcow2 \
+		-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+		-chardev pty,id=char0 -serial chardev:char0 ;
+else
+	qemu-system-x86_64 \
+		-m 1G \
+		-nographic \
+		-bios ./bios.bin \
+		-drive file=blabl-space-20250612.qcow2 \
+		-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+		-chardev pty,id=char0 -serial chardev:char0 ;
+fi
