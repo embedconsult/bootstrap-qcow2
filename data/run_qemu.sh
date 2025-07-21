ad63ba56f87c85667f1dcde59c5340ffec8bf338
jkridner <>
2025-07-21 00:22:00 +0000

    Built EFI Hello World in Rust for ARM as well
    FossilOrigin-Name: 380af3f907fd068bcd9bcbcb29806a99933004231bd4eeaa6bb0f006997161e9

--- data/run_qemu.sh
+++ data/run_qemu.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
+TARGET="${1:-x86_64}"
 cd $(dirname $0)
-if [ "$(uname -m)" == "x86_64" ]; then
-	qemu-system-x86_64 \
-		-cpu host -enable-kvm -smp 2 -m 1G \
-		-nographic \
-		-bios ./bios.bin \
-		-drive file=blabl-space-20250612.qcow2 \
-		-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
-		-chardev pty,id=char0 -serial chardev:char0 ;
+if [ "$TARGET" == "arm" ]; then
+	qemu-system-aarch64 \
+			-machine virt \
+			-m 1G \
+			-cpu max \
+			-nographic \
+			-drive file=biosarm.bin,format=raw,if=pflash,readonly=on \
+			-drive file=blabl-space-20250612.qcow2 \
+			-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+			-chardev pty,id=char0 -serial chardev:char0 ;
 else
-	qemu-system-x86_64 \
-		-m 1G \
-		-nographic \
-		-bios ./bios.bin \
-		-drive file=blabl-space-20250612.qcow2 \
-		-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
-		-chardev pty,id=char0 -serial chardev:char0 ;
+	if [ "$(uname -m)" == "x86_64" ]; then
+		qemu-system-x86_64 \
+			-cpu host -enable-kvm -smp 2 \
+			-m 1G \
+			-nographic \
+			-bios ./biosx86.bin \
+			-drive file=blabl-space-20250612.qcow2 \
+			-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+			-chardev pty,id=char0 -serial chardev:char0 ;
+	else
+		qemu-system-x86_64 \
+			-bios ./biosx86.bin \
+			-m 1G \
+			-nographic \
+			-drive file=blabl-space-20250612.qcow2 \
+			-netdev user,id=net0 -device virtio-net-pci,netdev=net0 \
+			-chardev pty,id=char0 -serial chardev:char0 ;
+	fi
 fi
