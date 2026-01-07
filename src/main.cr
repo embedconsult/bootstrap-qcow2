fc2281ba2f73e6cc333807a6c628a70fbae04b89
Jason Kridner <jkridner@beagleboard.org>
2026-01-07 16:52:46 +0000

    perform basic chroot setup in main.cr

--- src/main.cr
+++ src/main.cr
@@ -1,17 +1,18 @@
-require "option_parser"
+require "file_utils"
 require "./sysroot_builder"
 require "./sysroot_runner_lib"
 
 module Bootstrap
   class Main
     def self.run
+      workspace = Path["data/sysroot"]
       architecture = SysrootBuilder::DEFAULT_ARCH
       branch = SysrootBuilder::DEFAULT_BRANCH
       base_version = SysrootBuilder::DEFAULT_BASE_VERSION
 
       puts "Sysroot builder log level=#{Log.for("").level} (env-configured)"
       builder = SysrootBuilder.new(
-        workspace: Path["data/sysroot"],
+        workspace: workspace,
         architecture: architecture,
         branch: branch,
         base_version: base_version,
@@ -22,6 +23,31 @@ module Bootstrap
       )
       chroot_path = builder.generate_chroot(include_sources: true)
       puts "Prepared chroot directory at #{chroot_path}"
+
+      begin
+        FileUtils.mkdir_p(workspace / "rootfs/dev")
+        Process.run("mount", ["-t", "devtmpfs", "none", (workspace / "rootfs/dev").to_s])
+
+        FileUtils.mkdir_p(workspace / "rootfs/proc")
+        Process.run("mount", ["-t", "proc", "none", (workspace / "rootfs/proc").to_s])
+
+        FileUtils.mkdir_p(workspace / "rootfs/sys")
+        Process.run("mount", ["-t", "sysfs", "none", (workspace / "rootfs/sys").to_s])
+
+        FileUtils.mkdir_p(workspace / "rootfs/tmp")
+        Process.run("mount", ["-t", "tmpfs", "none", (workspace / "rootfs/tmp").to_s])
+
+        File.write(workspace / "rootfs/etc/resolv.conf", "nameserver 8.8.8.8", perm = 0o644)
+
+        Process.chroot((workspace / "rootfs").to_s)
+        Process.run("/bin/echo", ["got here"], output: STDOUT)
+        Process.run("apk", ["add", "crystal", "clang", "lld"], output: STDOUT)
+      ensure
+        Process.run("umount", ["-l", (workspace / "rootfs/tmp").to_s])
+        Process.run("umount", ["-l", (workspace / "rootfs/sys").to_s])
+        Process.run("umount", ["-l", (workspace / "rootfs/proc").to_s])
+        Process.run("umount", ["-l", (workspace / "rootfs/dev").to_s])
+      end
     end
   end
 end
