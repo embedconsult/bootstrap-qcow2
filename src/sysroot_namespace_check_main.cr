24ab3cee7eca687e9d0b3d3fd45cbfd4e3ede749
Jason Kridner <jkridner@beagleboard.org>
2026-01-11 07:57:54 +0000

    bad check for sysgroups still

--- src/sysroot_namespace_check_main.cr
+++ src/sysroot_namespace_check_main.cr
@@ -8,20 +8,17 @@ module Bootstrap
     def self.run
       proc_root = Path["/proc"]
       filesystems_path = Path["/proc/filesystems"]
-      setgroups_path = "/proc/self/setgroups"
 
       OptionParser.parse do |parser|
         parser.banner = "Usage: crystal run src/sysroot_namespace_check_main.cr -- [options]"
         parser.on("--proc-root=PATH", "Override proc root (default: #{proc_root})") { |val| proc_root = Path[val] }
         parser.on("--filesystems=PATH", "Override /proc/filesystems path (default: #{filesystems_path})") { |val| filesystems_path = Path[val] }
-        parser.on("--setgroups=PATH", "Override /proc/self/setgroups path (default: #{setgroups_path})") { |val| setgroups_path = val }
         parser.on("-h", "--help", "Show this help") { puts parser; exit }
       end
 
       restrictions = SysrootNamespace.collect_restrictions(
         proc_root: proc_root,
         filesystems_path: filesystems_path,
-        setgroups_path: setgroups_path
       )
 
       if restrictions.empty?
