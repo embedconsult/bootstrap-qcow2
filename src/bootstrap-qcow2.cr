c17c77074b4353011e14d54cfbab46a9e7af0830
Jason Kridner <jkridner@beagleboard.org>
2026-01-30 04:58:00 +0000

    0.1.2

--- src/bootstrap-qcow2.cr
+++ src/bootstrap-qcow2.cr
@@ -8,7 +8,7 @@ require "log"
 
 module Bootstrap
   # Semantic version of the bootstrap-qcow2 tooling.
-  VERSION = "0.1.1"
+  VERSION = "0.1.2"
 
   # Basic qcow2 wrapper that validates tools and triggers image builds.
   #
